0870fc01debec79355a783a65bb5a6ee82731cb4
Ruth Zhang <ruthzhang713@gmail.com>
2024-02-10 19:49:59 -0800

    changes

--- App.js
+++ App.js
@@ -1,1 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from "./Screen/HomeScreen"
+import SettingsScreen from "./Screen/SettingsScreen"
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tab = createBottomTabNavigator();
+export default function App() {
+  return ( <>
+    <NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View> </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+
 
